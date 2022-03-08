9da6fdd17a01fd735d1fa58848fef02452474921
sergycg <sergycg@gmail.com>
2022-03-08 14:44:16 +0100

    first commit

---
+++ src/main/java/com/formacionbdi/springboot/app/gateway/security/SpringSecurityConfig.java
@@ -0,0 +1,62 @@
+package com.formacionbdi.springboot.app.gateway.security;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+	
+	@Autowired
+	private JwtAuthenticationFilter authenticationFilter;
+
+	@Bean
+	public SecurityWebFilterChain configure(ServerHttpSecurity http) {
+		return http.authorizeExchange()
+				.pathMatchers("/api/security/oauth/**", "/api/arduino/voltaje/**", "/api/aws/s3/**").permitAll()
+				.pathMatchers(HttpMethod.GET, "/api/recetas/**", "/api/usuarios/usuarios").permitAll()
+				.pathMatchers(HttpMethod.GET, "/api/usuarios/usuarios/{id}").hasAnyRole("ADMIN", "USER")
+				.pathMatchers("/api/usuarios/**").hasRole("ADMIN")
+				.pathMatchers("/api/recetas/**").hasAnyRole("ADMIN", "USER")
+				.anyExchange().authenticated()
+				.and().addFilterAt(authenticationFilter, SecurityWebFiltersOrder.AUTHENTICATION)
+				.csrf().disable()
+				.build();
+	}
+	
+
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+    public CorsWebFilter corsWebFilter() {
+
+        final CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+//        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        corsConfig.setAllowCredentials(true);
+        corsConfig.addAllowedHeader("*");
+//        corsConfig.addAllowedHeader("Content-Type");
+//        corsConfig.addAllowedHeader("Authorization");
+     
+        
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//		source.registerCorsConfiguration("/api/security/oauth/**", corsConfig2);
+//		source.registerCorsConfiguration("/api/recetas/**", corsConfig);
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
+    }
+	
+}
